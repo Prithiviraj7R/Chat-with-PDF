b909b89daba5c3fe3c990c79e70cc8378a6b70ac
Prithiviraj-7 <prithiviraj.iitm@gmail.com>
2024-02-22 21:32:00 +0530

    Document to Vector modules

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from dotenv import load_dotenv
 
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+
 
 def get_pdf_content(documents):
     raw_text = ""
@@ -12,6 +16,24 @@ def get_pdf_content(documents):
             raw_text += page.extract_text()
 
     return raw_text
+
+
+def get_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    text_chunks = text_splitter.split_text(text)
+    return text_chunks
+
+
+def get_embeddings(chunks):
+    embeddings = OpenAIEmbeddings()
+    vector_storage = FAISS.from_texts(texts=chunks, embedding=embeddings)
+
+    return vector_storage
 
 
 def main():
@@ -30,9 +52,12 @@ def main():
         )
         if st.button("Run"):
             with st.spinner("Processing..."):
-                ## extract text from pdf documents
+                # extract text from pdf documents
                 extracted_text = get_pdf_content(documents)
-                st.write(extracted_text)
+                # convert text to chunks of data
+                text_chunks = get_chunks(extracted_text)
+                # create vector embeddings
+                vector_embeddings = get_embeddings(text_chunks)
 
 
 if __name__ == "__main__":
